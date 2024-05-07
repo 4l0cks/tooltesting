71ecb5ec555bfca6d7c05d6cc95aa86ca2014048
florian <kassi.florian@gmail.com>
2024-05-07 12:35:58 +0200

    Project used for tets

---
+++ src/main/java/com/tooltesting/TooltestingApplication.java
@@ -0,0 +1,13 @@
+package com.tooltesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooltestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TooltestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tooltesting/TooltestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tooltesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TooltestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
